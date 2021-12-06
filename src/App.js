fd965cd53818edaa4d842ea5a23ef1a043d0a5f0
Kingsley <kingsleyyong0208@gmail.com>
2021-12-06 14:32:56 +0800

    Uodate to neccesary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+   let arr = ['blue', 'red', 'green'],
+      obj = { name: 'lalala', height: 35 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div>
+         {' '}
+         Hello React Here! {arr} {obj.name}
+      </div>
+   )
 }
 
-export default App;
+export default App
